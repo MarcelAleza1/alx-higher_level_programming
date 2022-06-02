2958d0e39d69499a4092255f342281d85dad5ddd
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-06-02 09:45:59 -0700

    add

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-@!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    args_num = len(argv)
-    sum = 0
-    for i in range(1, args_num):
-        sum += int(argv[i])
-    print("{:d}".format(sum))
