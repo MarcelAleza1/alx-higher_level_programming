3e53438823329a9ce39e0886fd74d1ac7be48236
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-06-08 12:16:33 -0700

    sum

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,4 +2,6 @@
 def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
-    return sum([x*y for (x, y) in my_list]) / sum([y for (x,y) in my_list])
+    sum1 = sum([x * y for (x, y) in my_list])
+    sum2 = sum([y for (x, y) in my_list])
+    return sum1 / sum2
