6e5d62797302713228a4e6488acb85eaf535b736
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-06-08 12:21:48 -0700

    fisced

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    retrun list(map(lambda x: list(lamda y: y ** 2, x)),matrix))
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def mulitply_by_2(my_dict):
-    dict1 = {}
-    for x, y in my_dict.items():
-        dict1.update({x: (y * 2)})
-    return dict1
