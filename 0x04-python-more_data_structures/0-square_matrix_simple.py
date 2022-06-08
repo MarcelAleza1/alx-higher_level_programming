a629c749b4b61dc1fa61d0dbf428c6dc0a281881
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-06-08 11:27:49 -0700

    square

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,8 +4,7 @@ def squre_matrix_simple(matrix=[]):
     function to compare the square
     value of all integers of a matrix.
     """
-    mat = []
-    for column in matrix:
-        result = list(map(lambda val: val**2, column))
-        mat.append(result)
-    return mat
+    result = []
+    for x in matrix:
+        result.append([p**2 for p in x])
+    return result
