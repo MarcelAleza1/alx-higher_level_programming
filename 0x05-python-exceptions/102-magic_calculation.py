d12384c2fff1d57584b5132a57dc2cd3b8e37955
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-06-20 14:17:05 -0700

    exceptions

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+def magic_calculation(a, b):
+    x = 0
+    for i in range(1, 3):
+        try:
+            if (i > a):
+                raise Exception('Too far')
+            else:
+                x += (a**b)/i
+        except:
+            x = (b + a)
+            break
+    return x
