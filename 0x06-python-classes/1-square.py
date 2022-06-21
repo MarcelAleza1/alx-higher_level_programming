c398729d2ed688ddce58e933bb79c00420ea0553
Mazi Essoloani Aleza <alezamaziessoloani@gmail.com>
2022-06-21 14:20:30 -0700

    define square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class called Square."""
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize the Square.
+
+        Args:
+            size (int): size of the square.
+        """
+
+        self._size = size
